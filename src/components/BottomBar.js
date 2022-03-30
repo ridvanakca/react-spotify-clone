a87d7ab18f6177c554b2cd51d4ba504ac41eed86
Rıdvan Akca <akca.ridvann@gmail.com>
2022-03-31 00:42:58 +0300

    logo and menu component is added to sidebar

--- src/components/BottomBar.js
+++ src/components/BottomBar.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function BottomBar() {
-  return <div className='h-24'>
+  return <div className='h-24 bg-footer border-t border-white border-opacity-5'>
     BottomBar
     </div>;
 }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,8 +1,14 @@
 import React from "react";
+import logo from 'img/logo.svg';
+import Menu from "./Sidebar/Menu";
 
 function Sidebar() {
-  return <aside className='w-60'>
-    Sidebar
+  return <aside className='w-60 py-6 flex flex-col'>
+    <a href='#' className='mb-7 px-6'>
+      <img className='h-10' src={logo} alt='' />
+    </a>
+    
+    <Menu />
     </aside>;
 }
 
