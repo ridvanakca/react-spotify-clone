5a3cbde30322f162a61b72b0c6c3fb7be70190b0
Rıdvan Akca <akca.ridvann@gmail.com>
2022-04-11 12:15:31 +0300

    search component in navbar is added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,21 @@
-import React from 'react'
-import Auth from './Navbar/Auth'
-import Navigation from './Navbar/Navigation'
-
+import React from "react";
+import { useMatch } from "react-router";
+import Search from "./Navbar/Search";
+import Auth from "./Navbar/Auth";
+import Navigation from "./Navbar/Navigation";
 
 function Navbar() {
+  const searchRoute = useMatch("/search");
+
   return (
     <nav className='h-[3.75rem] flex items-center justify-between px-8'>
       <Navigation />
 
+      {searchRoute && <Search />}
+
       <Auth />
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
